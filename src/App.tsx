226ac804005ef580eaaad3213b5319012cb1a1c1
yxq <yanxiangqunwork@163.com>
2021-11-28 22:14:44 +0800

    feat: 去除首页的 notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Layout, notification } from 'antd';
+import { Layout } from 'antd';
 import umbrella from 'umbrella-storage';
 import { useAlita } from 'redux-alita';
 import Routes from './routes';
@@ -36,45 +36,6 @@ function handleResize(handler: (isMobile: boolean) => void) {
     window.addEventListener('resize', resizeListener.bind(null, handler));
 }
 
-function openFNotification() {
-    const openNotification = () => {
-        notification.open({
-            message: '博主-yezihaohao',
-            description: (
-                <div>
-                    <p>
-                        GitHub地址：
-                        <a
-                            href="https://github.com/yezihaohao"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            https://github.com/yezihaohao
-                        </a>
-                    </p>
-                    <p>
-                        博客地址：
-                        <a
-                            href="https://yezihaohao.github.io/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            https://yezihaohao.github.io/
-                        </a>
-                    </p>
-                </div>
-            ),
-            icon: <SmileOutlined style={{ color: 'red' }} />,
-            duration: 0,
-        });
-        umbrella.setLocalStorage('hideBlog', true);
-    };
-    const storageFirst = umbrella.getLocalStorage('hideBlog');
-    if (!storageFirst) {
-        openNotification();
-    }
-}
-
 /**
  * 获取服务端异步菜单
  * @param handler 执行回调
@@ -92,6 +53,7 @@ function fetchSmenu(handler: any) {
 }
 
 const App = (props: AppProps) => {
+    console.log('render app');
     const [collapsed, setCollapsed] = useState<boolean>(false);
     const [auth, responsive, setAlita] = useAlita(
         { auth: { permissions: null } },
@@ -105,7 +67,6 @@ const App = (props: AppProps) => {
         setAlita('responsive', { isMobile: checkIsMobile() });
 
         handleResize((isMobile: boolean) => setAlita('responsive', { isMobile }));
-        openFNotification();
         fetchSmenu((smenus: any[]) => setAlita('smenus', smenus));
     }, [setAlita]);
 
